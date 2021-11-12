86a9bc97e4ccb66a5d26c07fe9fc00c3e5e12485
Luigi Starace <luigi.starace@gmail.com>
2021-11-12 10:43:30 +0100

    coded the app

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/greet', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/fortune', (req, res) => {
+    var fortune;
+    min = 1;
+    max = 4;
+    random = Math.floor(Math.random() * (max - min) + min);
+    switch(random){
+        case 1:
+            fortune = "Great fortune! :)"
+            break;
+        case 2:
+            fortune = "Meh, average fortune today :/"
+            break;
+        case 3:
+            fortune = "Awful fortune today! Be careful! :("
+            break;
+    }
+    res.send(fortune)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
