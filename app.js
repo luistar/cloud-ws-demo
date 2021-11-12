8be341ef49928649fa0fee2ec986bea1b8ae6abe
Luigi Starace <luigi.starace@gmail.com>
2021-11-12 11:04:26 +0100

    fixed indentation

--- app.js
+++ app.js
@@ -7,23 +7,23 @@ app.get('/greet', (req, res) => {
 })
 
 app.get('/fortune', (req, res) => {
-    var fortune;
-    min = 1;
-    max = 4;
-    random = Math.floor(Math.random() * (max - min) + min);
-    switch(random){
-        case 1:
-            fortune = "Great fortune! :)"
-            break;
-        case 2:
-            fortune = "Meh, average fortune today :/"
-            break;
-        case 3:
-            fortune = "Awful fortune today! Be careful! :("
-            break;
-    }
-    res.send(fortune)
-  })
+  var fortune;
+  min = 1;
+  max = 4;
+  random = Math.floor(Math.random() * (max - min) + min);
+  switch(random){
+      case 1:
+          fortune = "Great fortune! :)"
+          break;
+      case 2:
+          fortune = "Meh, average fortune today :/"
+          break;
+      case 3:
+          fortune = "Awful fortune today! Be careful! :("
+          break;
+  }
+  res.send(fortune)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
